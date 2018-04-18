9abd0facd91c60989113ffbcda9c362f0226ec8b
Louis Chan <louischan@oursky.com>
2018-04-18 22:09:34 +0800

    Implement the Token Bucket algorithm

---
+++ pkg/server/ratelimit/ratelimit.go
@@ -0,0 +1,142 @@
+// Copyright 2015-present Oursky Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ratelimit
+
+import (
+	"time"
+)
+
+// Rate presents the rate.
+// For example, Rate{Limit: 5, Duration: 5 * time.Minute} means
+// 5 requests per 5 minutes.
+type Rate struct {
+	Limit    int64
+	Duration time.Duration
+}
+
+// RateLimiter provides rate limiting
+type RateLimiter interface {
+	// Allow is AllowAt(id, time.Now().UTC()).
+	Allow(id string) (bool, error)
+	// AllowAt determines whether the request identified by id received at t
+	// is allowed to proceed.
+	// The id can be as simple as the IP address of the HTTP request,
+	// or a concatenation of the IP address and the URL path.
+	// An error is returned when the implementation's store
+	// encounters an I/O error.
+	AllowAt(id string, t time.Time) (bool, error)
+}
+
+// TokenBucketInfo represents a bucket used by TokenBucket.
+type TokenBucketInfo struct {
+	// Tokens is the number of remaining tokens in this bucket.
+	// If the value is zero, then the limit is exceeded.
+	Tokens float64
+	// LastRequestedAt stores the last time this bucket has its
+	// tokens decremented.
+	// The algorithm uses this and a reference time to refill
+	// tokens.
+	LastRequestedAt time.Time
+}
+
+// TokenBucketStore contains storage operations required by TokenBucket.
+type TokenBucketStore interface {
+	// Get retrieves the info from the store.
+	Get(id string) (TokenBucketInfo, bool, error)
+	// Set saves the info to the store.
+	Set(id string, info TokenBucketInfo) error
+}
+
+// TokenBucket implements the Token Bucket algorithm.
+// Since it relies on two non-atomic storage operation,
+// it suffers from data race.
+// See https://en.wikipedia.org/wiki/Token_bucket
+type TokenBucket struct {
+	Store TokenBucketStore
+	Rate  Rate
+}
+
+func (p *TokenBucket) AllowAt(id string, t time.Time) (bool, error) {
+	info, ok, err := p.Store.Get(id)
+	if err != nil {
+		return false, err
+	}
+	maxTokens := p.tokensFromRate()
+	if !ok {
+		// If the info is not found, we initialize a new one with
+		// filled tokens.
+		info = TokenBucketInfo{
+			Tokens:          maxTokens,
+			LastRequestedAt: t,
+		}
+	} else {
+		// Otherwise we refill the tokens based on the difference between
+		// t and LastRequestedAt
+		tokensToRefill := p.tokensFromDiff(t, info.LastRequestedAt)
+		tokens := info.Tokens
+		tokens += tokensToRefill
+		if tokens > maxTokens {
+			tokens = maxTokens
+		}
+		info.Tokens = tokens
+	}
+	// Consume a token if possible
+	ret := true
+	if info.Tokens < 1.0 {
+		ret = false
+	} else {
+		info.Tokens -= 1.0
+	}
+	info.LastRequestedAt = t
+	// Store the info
+	err = p.Store.Set(id, info)
+	if err != nil {
+		return false, err
+	}
+	return ret, nil
+}
+
+func (p *TokenBucket) Allow(id string) (bool, error) {
+	return p.AllowAt(id, time.Now().UTC())
+}
+
+func (p *TokenBucket) tokensFromRate() float64 {
+	return float64(p.Rate.Limit)
+}
+
+// Return how many tokens should be refilled per second
+func (p *TokenBucket) refillRate() float64 {
+	limit := float64(p.Rate.Limit)
+	seconds := p.Rate.Duration.Seconds()
+	if seconds == 0.0 {
+		return 0.0
+	}
+	return limit / seconds
+}
+
+func (p *TokenBucket) tokensFromDiff(t time.Time, lastRequestedAt time.Time) float64 {
+	// Ignore nonsense time
+	if t.IsZero() || lastRequestedAt.IsZero() {
+		return 0.0
+	}
+	duration := t.Sub(lastRequestedAt)
+	// Ignore time traveler
+	if duration < 0 {
+		return 0.0
+	}
+	seconds := duration.Seconds()
+	refillRate := p.refillRate()
+	return refillRate * seconds
+}
---
+++ pkg/server/ratelimit/ratelimit_test.go
@@ -0,0 +1,118 @@
+// Copyright 2015-present Oursky Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ratelimit
+
+import (
+	"testing"
+	"time"
+)
+
+type tokenBucketStore struct {
+	data map[string]TokenBucketInfo
+}
+
+func (s *tokenBucketStore) Get(id string) (TokenBucketInfo, bool, error) {
+	info, ok := s.data[id]
+	return info, ok, nil
+}
+
+func (s *tokenBucketStore) Set(id string, info TokenBucketInfo) error {
+	if s.data == nil {
+		s.data = map[string]TokenBucketInfo{}
+	}
+	s.data[id] = info
+	return nil
+}
+
+func TestTokenBucket(t *testing.T) {
+	// 6 requests per 1 minute.
+	// The refill rate is 0.1 token per second.
+	rate := Rate{
+		Limit:    6,
+		Duration: 1 * time.Minute,
+	}
+	store := tokenBucketStore{}
+	var rateLimiter RateLimiter
+	tokenBucket := TokenBucket{
+		Store: &store,
+		Rate:  rate,
+	}
+	// Ensure TokenBucket implements RateLimiter
+	rateLimiter = &tokenBucket
+	timeline := []struct {
+		expected bool
+		t        time.Time
+	}{
+		// t = 0
+		{true, time.Date(2018, 1, 1, 0, 0, 0, 0, time.UTC)},
+		// t = 1
+		{true, time.Date(2018, 1, 1, 0, 0, 1, 0, time.UTC)},
+		// t = 2
+		{true, time.Date(2018, 1, 1, 0, 0, 2, 0, time.UTC)},
+		// t = 3
+		{true, time.Date(2018, 1, 1, 0, 0, 3, 0, time.UTC)},
+		// t = 4
+		{true, time.Date(2018, 1, 1, 0, 0, 4, 0, time.UTC)},
+		// t = 5
+		{true, time.Date(2018, 1, 1, 0, 0, 5, 0, time.UTC)},
+		// t = 6
+		{false, time.Date(2018, 1, 1, 0, 0, 6, 0, time.UTC)},
+		// t = 7
+		{false, time.Date(2018, 1, 1, 0, 0, 7, 0, time.UTC)},
+		// t = 8
+		{false, time.Date(2018, 1, 1, 0, 0, 8, 0, time.UTC)},
+		// t = 9
+		{false, time.Date(2018, 1, 1, 0, 0, 9, 0, time.UTC)},
+		// t = 10.0001; 10 seconds elapsed, 1 token was refilled
+		{true, time.Date(2018, 1, 1, 0, 0, 10, 1000, time.UTC)},
+		// t = 71; 1 minute elapsed, 6 tokens was refilled
+		{true, time.Date(2018, 1, 1, 0, 0, 71, 0, time.UTC)},
+		// t = 72
+		{true, time.Date(2018, 1, 1, 0, 0, 72, 0, time.UTC)},
+		// t = 73
+		{true, time.Date(2018, 1, 1, 0, 0, 73, 0, time.UTC)},
+		// t = 74
+		{true, time.Date(2018, 1, 1, 0, 0, 74, 0, time.UTC)},
+		// t = 75
+		{true, time.Date(2018, 1, 1, 0, 0, 75, 0, time.UTC)},
+		// t = 76
+		{true, time.Date(2018, 1, 1, 0, 0, 76, 0, time.UTC)},
+		// t = 76.0001
+		{false, time.Date(2018, 1, 1, 0, 0, 76, 1000, time.UTC)},
+		// t = 3677; 1 hour elapsed, 6 tokens was refilled
+		{true, time.Date(2018, 1, 1, 0, 0, 3677, 0, time.UTC)},
+		// t = 3678
+		{true, time.Date(2018, 1, 1, 0, 0, 3678, 0, time.UTC)},
+		// t = 3679
+		{true, time.Date(2018, 1, 1, 0, 0, 3679, 0, time.UTC)},
+		// t = 3680
+		{true, time.Date(2018, 1, 1, 0, 0, 3680, 0, time.UTC)},
+		// t = 3681
+		{true, time.Date(2018, 1, 1, 0, 0, 3681, 0, time.UTC)},
+		// t = 3682
+		{true, time.Date(2018, 1, 1, 0, 0, 3682, 0, time.UTC)},
+		// t = 3682.0001
+		{false, time.Date(2018, 1, 1, 0, 0, 3682, 1000, time.UTC)},
+	}
+	for _, item := range timeline {
+		actual, err := rateLimiter.AllowAt("id", item.t)
+		if err != nil {
+			t.Error("unexpected error", err)
+		}
+		if actual != item.expected {
+			t.Error("unexpected", actual, item.expected, item.t)
+		}
+	}
+}
