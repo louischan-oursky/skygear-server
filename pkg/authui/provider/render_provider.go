f219c2d2271f2eb94913fd0daed5c839b624673a
Louis Chan <louischan@oursky.com>
2020-02-17 19:33:22 +0800

    Set x_login_id_type[_has_{phone|text}]

--- pkg/authui/provider/render_provider.go
+++ pkg/authui/provider/render_provider.go
@@ -2,10 +2,12 @@ package provider
 
 import (
 	"net/http"
+	"net/url"
 
 	"github.com/skygeario/skygear-server/pkg/core/config"
 )
 
 type RenderProvider interface {
+	PrevalidateForm(values url.Values)
 	WritePage(w http.ResponseWriter, templateType config.TemplateItemType, data map[string]interface{})
 }
